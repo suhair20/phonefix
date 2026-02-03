14e30e78aa333121e17542d0a41caa462a58a62d
suhair20 <moidheensuhair@gmail.com>
2026-02-03 22:08:52 +0530

    chnges-s

--- frontend/slices/AdminSlice.js
+++ frontend/slices/AdminSlice.js
@@ -5,7 +5,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 const BASE_URL =
   import.meta.env.DEV
     ? "http://localhost:5000"
-    : "https://phonefix.onrender.com";
+    : "https://phonefix-i73f.onrender.com";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
