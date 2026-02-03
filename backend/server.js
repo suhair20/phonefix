ae94675a88ff725cfffce11e2f3399dbd6ed00d6
suhair20 <moidheensuhair@gmail.com>
2026-02-03 21:38:15 +0530

    gin-in

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   
   origin:['http://localhost:5173',
-    'https://lobuy.vercel.app/',
+    'https://lobuy.vercel.app',
     ] ,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
